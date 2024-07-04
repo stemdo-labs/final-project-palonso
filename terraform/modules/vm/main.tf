9c6f549602cb7730417e76bc42a96b1b05846b3e
Pilar_Alonso <palonso@stemdo.io>
2024-07-04 10:30:29 +0200

    feat: change version

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_virtual_machine" "vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 
