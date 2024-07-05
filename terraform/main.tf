aea5c50a027915ffd05e95d72c94b23c8d1b74cd
Pilar_Alonso <palonso@stemdo.io>
2024-07-05 14:23:12 +0200

    change terraform2

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,7 +80,7 @@ resource "azurerm_container_registry" "acrDev" {
 }
 # Azure Container Registry (ACR) prod
 resource "azurerm_container_registry" "acrprod" {
-  name                = "palonsoARProd"
+  name                = "palonsoARrProd"
   resource_group_name = azurerm_resource_group.main.name
   location            = var.location
   sku                 = "Basic"
